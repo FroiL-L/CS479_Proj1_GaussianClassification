f1bc44c98649a75634143ce4c19221d944039fb6
Froilan Luna <froil@DESKTOP-750FS49.localdomain>
2023-02-27 15:11:56 -0800

    Refactored data generation and file structure

---
+++ classification.hpp
@@ -0,0 +1,14 @@
+/* classification.hpp:
+ * 	A library that contains tools for classification.
+ * authors:
+ *
+ * date:
+ * 	27 February 2023
+ */
+
+#ifdef _CLASSIFICATION_H_
+#define _CLASSIFICATION_H_
+
+
+
+#endif
