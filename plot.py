7fb15f4c7d09764a2cf78f949685493e64414ddf
ramirezaaronjob <aaronjobr@nevada.unr.edu>
2023-03-01 21:25:09 -0800

    plotting

--- plot.py
+++ plot.py
@@ -3,7 +3,13 @@ import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
 
 # read input data from file
-X = np.loadtxt("B_BC.txt",dtype=float)
+X = np.loadtxt("B_BC.txt", dtype=float)
+
+# select 500 rows with ID 1 and 500 rows with ID 2 from X
+X_id1 = X[X[:, 2] == 1][:500]
+X_id2 = X[X[:, 2] == 2][:500]
+X = np.concatenate([X_id1, X_id2], axis=0)
+Y = X[:, 1]
 
 # Bayes classifier parameters
 mu1 = np.array([1, 1])
@@ -19,21 +25,25 @@ x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
 y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 
-# evaluate the probability densities on the mesh grid
-Z1 = multivariate_normal.pdf(np.c_[xx.ravel(), yy.ravel()], mean=mu1, cov=cov1)
-Z1 = Z1.reshape(xx.shape)
-
-Z2 = multivariate_normal.pdf(np.c_[xx.ravel(), yy.ravel()], mean=mu2, cov=cov2)
-Z2 = Z2.reshape(xx.shape)
+# evaluate the probability densities at (5, 5)
+Z1 = multivariate_normal.pdf([5, 5], mean=mu1, cov=cov1)
+Z2 = multivariate_normal.pdf([5, 5], mean=mu2, cov=cov2)
 
 # calculate the decision boundary
 Z = Z1 - Z2
 
 # plot the decision boundary
-plt.contourf(xx, yy, Z, levels=[-1, 0, 1], colors=('blue', 'red'), alpha=0.2)
+x = np.linspace(-10, 10, 100)
+y = x + 5  # slope = 1, y-intercept = 5
+
+plt.plot(x, y, '-r', label='y=x+5')
+plt.plot(5, 5, 'bo', label='(5,5)')
+
+#for plot B
+##plt.contourf(xx, yy, Z, levels=[-1, 0, 1], colors=('blue', 'red'), alpha=0.2)
 
 # create a color map for the data points
-colors = ['blue'] * int(len(X)/2) + ['red'] * int(len(X)/2)
+colors = np.concatenate([np.array(['blue'] * 500), np.array(['red'] * 500)])
 
 # plot the data points
 plt.scatter(X[:, 0], X[:, 1], c=colors, cmap=plt.cm.coolwarm, edgecolors='black')
